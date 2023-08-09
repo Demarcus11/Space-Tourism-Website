f770ea9f1f9e4d94c9095162b75ebff75495636d
Demarcus Hollman <dhollman1@gmail.com>
2023-08-09 16:14:03 -0400

    add react helmet to change bg and title for each page

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -1,3 +1,17 @@
-export default function Destination() {
-  return <h1>Destination Page</h1>;
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+function Destination() {
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>Space Tourism | Destination</title>
+          <body className="destination"></body>
+        </Helmet>
+        <h1>Destination Page</h1>
+      </HelmetProvider>
+    </>
+  );
 }
+
+export default Destination;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,37 @@
-export default function Home() {
-  return (
-    <main className="home | grid-container grid-container--home">
-      <div>
-        <h1 className="home__title">
-          So, You Want To Travel To <span>Space</span>
-        </h1>
-        <p>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
-      </div>
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { NavLink } from "react-router-dom";
 
-      <div>
-        <a className="home__link" href="#">
-          Explore
-        </a>
-      </div>
-    </main>
+function Home() {
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>Space Tourism | Home</title>
+          <body className="home"></body>
+        </Helmet>
+
+        <main className="home | grid-container grid-container--home">
+          <div>
+            <h1 className="home__title">
+              So, You Want To Travel To <span>Space</span>
+            </h1>
+            <p className="home__text">
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+
+          <div>
+            <NavLink to="/destination" className="home__link">
+              Explore
+            </NavLink>
+          </div>
+        </main>
+      </HelmetProvider>
+    </>
   );
 }
+
+export default Home;
