3e8a17862ae2ed9f87b255446142497237d7ea6d
Demarcus Hollman <dhollman1@gmail.com>
2023-08-09 17:47:15 -0400

    add destination styles

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -8,7 +8,71 @@ function Destination() {
           <title>Space Tourism | Destination</title>
           <body className="destination"></body>
         </Helmet>
-        <h1>Destination Page</h1>
+        <main className="destination | grid-container grid-container--destination">
+          <h1 className="destination__title">
+            <span>01</span>
+            Pick Your Destination
+          </h1>
+
+          <picture className="destination__image">
+            <source
+              srcSet="/assets/destination/image-moon.webp"
+              type="image/webp"
+            />
+            <img src="/assets/destination/image-moon.png" alt="the moon" />
+          </picture>
+
+          <div className="tabs">
+            <div>
+              <input id="tab-1" name="planet" type="radio" />
+              <label className="tab" htmlFor="tab-1">
+                Moon
+              </label>
+            </div>
+            <div>
+              <input id="tab-2" name="planet" type="radio" />
+              <label className="tab" htmlFor="tab-2">
+                Mars
+              </label>
+            </div>
+            <div>
+              <input id="tab-3" name="planet" type="radio" />
+              <label className="tab" htmlFor="tab-3">
+                Europa
+              </label>
+            </div>
+            <div>
+              <input id="tab-4" name="planet" type="radio" />
+              <label className="tab" htmlFor="tab-4">
+                Titan
+              </label>
+            </div>
+          </div>
+
+          <article>
+            <div>
+              <h2 className="destination__category">Moon</h2>
+              <p className="destination__text">
+                See our planet as you’ve never seen it before. A perfect
+                relaxing trip away to help regain perspective and come back
+                refreshed. While you’re there, take in some history by visiting
+                the Luna 2 and Apollo 11 landing sites.
+              </p>
+            </div>
+
+            <div className="destination__stats">
+              <div>
+                <h3>Avg. Distance</h3>
+                <p>384,400 Km</p>
+              </div>
+
+              <div>
+                <h3>Est. Travel Time</h3>
+                <p>3 Days</p>
+              </div>
+            </div>
+          </article>
+        </main>
       </HelmetProvider>
     </>
   );
