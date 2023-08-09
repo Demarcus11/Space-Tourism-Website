3da93d8c72706822cf9980dcf6924709ce969227
Demarcus Hollman <dhollman1@gmail.com>
2023-08-09 13:53:08 -0400

    add home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,22 @@
 export default function Home() {
   return (
-    <main>
+    <main className="home | grid-container grid-container--home">
       <div>
-        <div>
-          <h1>
-            <span>So, You Want To Travel To</span> Space
-          </h1>
-          <p>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </div>
+        <h1 className="home__title">
+          So, You Want To Travel To <span>Space</span>
+        </h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
 
-        <div>
-          <a href="#">Explore</a>
-        </div>
+      <div>
+        <a className="home__link" href="#">
+          Explore
+        </a>
       </div>
     </main>
   );
